177ff7f644b40efd47c0402b82c719079c5454a3
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-11-28 20:05:38 +0800

    Add background color to Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Features from "@/components/Features";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-start bg-zinc-50 font-sans dark:bg-black">
+    <div className="flex min-h-screen flex-col items-center justify-start py-2 bg-linear-to-b from-primary-green to-accent-green text-accent-white">
       <main className="flex-1 w-full flex flex-col items-center gap-20 sm:items-start">
         <nav className="w-full flex justify-center border-b border-b-foreground/10 bg-primary-green h-20">
           <NavBar />
