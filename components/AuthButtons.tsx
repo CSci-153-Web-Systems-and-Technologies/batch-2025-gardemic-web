b22839ae736b9f18db08d4f9ce83552f46020d3c
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-11-28 20:24:29 +0800

    Improve the UI of the Decision Screen

--- components/AuthButtons.tsx
+++ components/AuthButtons.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 
 export default function AuthButtons() {
 
+    const buttonAttributes = "bg-secondary-green text-black font-semibold hover:bg-accent-green hover:text-accent-white";
+
     return (
-        <div className="flex flex-col gap-4 font-montserrat font-semibold text-xl">
-            <Button asChild size={"lg"} variant={"default"}>
+        <div className="flex flex-col gap-4 font-montserrat text-xl">
+            <Button asChild size={"lg"} variant={"default"} className={buttonAttributes}>
                 <Link href="/login">Sign into your Account</Link>
             </Button>
 
-            <Button asChild size={"lg"} variant={"default"}>
+            <Button asChild size={"lg"} variant={"default"} className={buttonAttributes}>
                 <Link href="/create-account">Create Account</Link>
             </Button>
         
