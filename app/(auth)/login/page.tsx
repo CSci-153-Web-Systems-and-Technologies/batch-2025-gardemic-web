aedef7086ad771af6ac2e7ebd142cc6e24ce4cdc
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-11-29 18:22:49 +0800

    Add Login Form to login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import AuthBackground from '@/public/auth-bg-image.png';
 
+import { LoginForm } from './_components/LoginForm';
+
 export default function Login() {
   return (
     <div className="grid grid-cols-4 h-screen">
@@ -24,11 +26,11 @@ export default function Login() {
           fill
           className='object-cover'
         />
-        <div className="absolute inset-0 bg-black/30 z-10"></div>
+        <div className="absolute inset-0 bg-black/35 z-10"></div>
       </div>
       
       <div className='bg-accent-white flex col-span-2'>
-      
+        <LoginForm className="m-auto" />
       </div>
     </div>
   );
