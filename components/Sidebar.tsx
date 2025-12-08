ca46d3489f7339bb392e3fc274eaeb7b76636a6d
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-12-08 22:36:24 +0800

    Modify Sidebar layout to include Sidebar components

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,62 @@
+import { useState } from "react";
+import { SidebarHeader } from "./SidebarHeader"
+import { SidebarToggle } from "./ui/sidebar-toggle"
+import { SidebarItem } from "./ui/sidebar-item";
+import { PrimarySidebarItems, SecondarySidebarItems } from "./SidebarItemConfig";
+import { useSidebarLogic } from "@/hooks/sidebar-logic"
+import { SidebarProfile } from "./SidebarProfile";
+
+import { MenuItemConfig } from "@/types";
+
 export default function Sidebar() {
+    const { isOpen, isLocked, toggleLock, containerProps } = useSidebarLogic();
+    const [activeItem, setActiveItem] = useState('Tasks');
+
+    const renderNavItems = (items: MenuItemConfig[]) => (
+    items.map((item) => (
+      <SidebarItem 
+        key={item.id}
+        icon={item.icon} 
+        label={item.label} 
+        isOpen={isOpen} 
+        isActive={activeItem === item.id}
+        onClick={() => setActiveItem(item.id)}
+      />
+    ))
+  );
+
     return (
         <>
-            <div className="bg-accent-green h-screen w-full flex flex-col">
-                <div className="">
-                    Sidebar
+            <aside
+                {...containerProps}
+                className={`
+                    relative h-screen bg-accent-green shadow-xl flex flex-col py-6 px-3
+                    transition-all duration-500 ease-[cubic-bezier(0.25,0.8,0.25,1)]
+                    ${isOpen ? 'w-64' : 'w-20'}
+                `}
+            >
+                <SidebarToggle isLocked={isLocked} onToggle={toggleLock} />
+
+                <SidebarHeader isOpen={isOpen} />
+
+                {/* --- Main Navigation --- */}
+                <div className="flex-1 overflow-y-auto overflow-x-hidden space-y-2 no-scrollbar">
+                    {renderNavItems(PrimarySidebarItems)}
                 </div>
-            </div>
+
+                <div className="my-4 border-t border-sage-600/50 w-full" />
+
+                {/* --- Secondary Navigation --- */}
+                <div className="mb-6 space-y-2">
+                    {renderNavItems(SecondarySidebarItems)}
+                </div>
+
+                <SidebarProfile 
+                    name="Froakie" 
+                    avatarUrl="https://picsum.photos/200" 
+                    isOpen={isOpen}
+                />
+            </aside>
         </>
     )
 }
