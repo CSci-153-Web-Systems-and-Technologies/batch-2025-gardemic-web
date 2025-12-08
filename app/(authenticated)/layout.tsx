f786a7103a97d59d767cff6002809124c4cb66b5
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-12-08 17:18:11 +0800

    Set-up initial layout for Sidebar in authenticated pages

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,13 +1,23 @@
 'use client'
 
+import Sidebar from "@/components/Sidebar";
+
 export default function ProtectedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <div className="font-aclonica">
-        {children}
-    </div>
+    <>
+      <div className="grid grid-cols-12">
+        
+        <div className="col-start-1 col-span-2">
+          <Sidebar />
+        </div>
+        <div className="font-aclonica col-start-3 col-span-9">
+          {children}
+        </div>
+      </div>        
+    </>
   );
 }
