6ccdb8d3bca5b20d0870b1041de4ec1b1ffc2022
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-12-08 17:17:18 +0800

    Initialize Sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+    return (
+        <>
+            <div className="bg-accent-green h-screen w-full flex flex-col">
+                <div className="">
+                    Sidebar
+                </div>
+            </div>
+        </>
+    )
+}
