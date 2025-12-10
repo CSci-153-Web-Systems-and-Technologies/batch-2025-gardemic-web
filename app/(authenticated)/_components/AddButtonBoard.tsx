ff69d9d92a8d113ddbbd87667796662d113e5a96
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-12-10 20:34:22 +0800

    Temporarily add logout button to button board

--- app/(authenticated)/_components/AddButtonBoard.tsx
+++ app/(authenticated)/_components/AddButtonBoard.tsx
@@ -1,14 +1,34 @@
 "use client";
 
+import { createClient } from "@/utils/supabase/client";
+import { useRouter } from "next/navigation";
 import { ActionButton } from "./ActionButton";
+import { useState } from "react";
 
 export default function ButtonBoard() {
+
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+    
     const handleAddTask = () => {
         alert('Add Task clicked!');
     }
 
     const handleAddGarden = () => {
         alert('Add Garden clicked!');
+    }
+
+    const handleLogout = async () => {
+        setIsLoading(true);
+        const supabase = createClient();
+
+        await supabase.auth.signOut();
+
+        router.refresh();
+
+        router.push("/");
+
+        setIsLoading(false);
     }
 
     return (
@@ -19,6 +39,10 @@ export default function ButtonBoard() {
 
             <ActionButton onClick={handleAddGarden}>
                 Add Garden
+            </ActionButton>
+
+            <ActionButton onClick={handleLogout} variant={"destructive"}>
+                {isLoading ? "Logging out..." : "Logout"}
             </ActionButton>
         </div>
     )
