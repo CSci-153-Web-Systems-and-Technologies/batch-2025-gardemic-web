b862699d1a3b029492b988c7b3f0f41a7b5ef7ff
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-12-10 14:01:48 +0800

    Modify ActionButton layout

--- app/(authenticated)/_components/ActionButton.tsx
+++ app/(authenticated)/_components/ActionButton.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
@@ -8,7 +10,7 @@ export function ActionButton({ className, children, ...props }: ActionButtonProp
     return (
         <Button 
             {...props}
-            className={cn("bg-primary-green hover:bg-accent-green text-white hover:text-black font-montserrat font-normal text-md px-4 py-6 rounded-md shadow-sm transition-colors"
+            className={cn("bg-primary-green hover:bg-accent-green text-white hover:text-black font-montserrat font-normal text-md px-4 py-5 rounded-md shadow-sm transition-colors"
             , className)}
         >
             {children}
