b135091e86f46f02f12e7e9e18d6c7cd79865bc4
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-12-10 21:32:55 +0800

    Create initial gardens page

---
+++ app/(authenticated)/gardens/page.tsx
@@ -0,0 +1,12 @@
+export default function GardensPage()
+{
+    return (
+        <>
+          <div className="bg-accent-white h-full">
+            <h1 className="text-2xl">
+                Gardens page
+            </h1>
+          </div>
+        </>
+    );
+}
