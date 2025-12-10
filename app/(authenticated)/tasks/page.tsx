e1ac32d71fe1b139a1176e46118acf61081b4e8b
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-12-10 13:45:31 +0800

    Initialize layout for task page content

--- app/(authenticated)/tasks/page.tsx
+++ app/(authenticated)/tasks/page.tsx
@@ -1,6 +1,14 @@
+import { ActionButton } from "../_components/ActionButton";
+
 export default function Protected()
 {
     return (
-        <h1>Protected Pages</h1>
+        <div className="flex flex-col h-38 bg-accent-white">
+            <div className="flex flex-row justify-between h-24 border-b-4 border-b-black/20">
+                <div className="text-3xl font-montserrat font-bold py-6.5 pl-6">
+                    Tasks
+                </div>
+            </div>
+        </div>
     );
 }
