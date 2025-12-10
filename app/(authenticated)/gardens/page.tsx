3f0c6261c2044fda19305429485273666c9d54aa
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-12-11 01:27:38 +0800

    Add Garden components w/ functionality into the page

--- app/(authenticated)/gardens/page.tsx
+++ app/(authenticated)/gardens/page.tsx
@@ -1,11 +1,34 @@
+"use client";
+
 import Description from "../_components/Description";
+import GardenList from "./_components/GardenList";
+import AddGardenButton from "./_components/AddGardenButton";
+import { AddGardenModal } from "../_components/AddGardenModel";
+import { useState } from "react";
 
 export default function GardensPage()
 {
+    const [isGardenModalOpen, setIsGardenModalOpen] = useState(false);
+    const [refreshTrigger, setRefreshTrigger] = useState(0);
+    
+    
+    const handleGardenAdded = () => {
+        setRefreshTrigger(prev => prev + 1);
+    };
+
     return (
         <>
           <div className="bg-accent-white h-full">
-            <Description title="My Gardens" subtitle="Organize your plants via garden spaces" />
+            <Description title="My Gardens" subtitle="Organize your plants via garden spaces"> 
+                <AddGardenButton onAddGardenClick={() => setIsGardenModalOpen(true)}/>
+            </Description>
+            <GardenList key={refreshTrigger}/>
+
+            <AddGardenModal 
+                isOpen={isGardenModalOpen}
+                onClose={() => setIsGardenModalOpen(false)}
+                onGardenAdded={handleGardenAdded}
+            />
           </div>
         </>
     );
