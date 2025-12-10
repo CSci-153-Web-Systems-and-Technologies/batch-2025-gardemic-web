326af0ec6d749107906b1501d150d10f028e3093
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-12-10 21:32:38 +0800

    Move task page header to layout file

--- app/(authenticated)/layout.tsx
+++ app/(authenticated)/layout.tsx
@@ -1,6 +1,9 @@
 'use client'
 
 import Sidebar from "@/components/Sidebar";
+import ButtonBoard from "./_components/AddButtonBoard";
+import Description from "./_components/Description";
+import { NavBoard } from "./_components/NavBoard";
 
 export default function ProtectedLayout({
   children,
@@ -12,6 +15,19 @@ export default function ProtectedLayout({
        <div className="flex h-screen overflow-hidden">
           <Sidebar />
           <main className="font-aclonica flex-1 overflow-y-auto">
+            <div className="flex flex-col bg-accent-white">
+                <div className="flex flex-row justify-between h-24 border-b-4 border-b-black/20">
+                    <div className="text-3xl font-montserrat font-bold py-6.5 pl-6">
+                        Tasks
+                    </div>
+
+                    <ButtonBoard />
+
+                </div>
+                <div className="w-full h-14 border-b-4 border-b-black/20">
+                    <NavBoard />
+                </div>
+            </div>
             {children}
           </main>
        </div>
--- app/(authenticated)/tasks/page.tsx
+++ app/(authenticated)/tasks/page.tsx
@@ -1,26 +1,8 @@
-import ButtonBoard from "../_components/AddButtonBoard";
-import Description from "../_components/Description";
-import { NavBoard } from "../_components/NavBoard";
-
-export default function Protected()
+export default function TaskPage()
 {
     return (
         <>
-            <div className="flex flex-col h-full bg-accent-white">
-                <div className="flex flex-row justify-between h-24 border-b-4 border-b-black/20">
-                    <div className="text-3xl font-montserrat font-bold py-6.5 pl-6">
-                        Tasks
-                    </div>
 
-                    <ButtonBoard />
-
-                </div>
-                <div className="w-full h-14 border-b-4 border-b-black/20">
-                    <NavBoard />
-                </div>
-
-                <Description title="Maintenance Tasks" subtitle="Track and complete your plant care tasks"/>
-            </div>
         </>
     );
 }
