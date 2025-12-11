ba4ae8a172c18b28cff3c8c69a58f26681bd4249
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-12-11 17:58:29 +0800

    Create JournalEntry table in Supabase and init type in NextJS

--- types/index.ts
+++ types/index.ts
@@ -88,6 +88,18 @@ export interface GardenPlant {
   added_at: string;
 }
 
+export interface JournalEntry {
+  journal_id: string;
+  user_id: string;
+  title: string;
+  content: string;
+  created_at: string;
+}
+
+export type GroupedEntries = {
+  [key: string]: JournalEntry[];
+};
+
 export interface GardenWithPlants extends Garden {
   garden_plants: {
     plant: Plant;
