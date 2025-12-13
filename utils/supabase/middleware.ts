618149772667b5e6a661a30347af990e2fb340d8
Lejohn Owen A. Loreto <23-1-00191@vsu.edu.ph>
2025-12-13 15:50:54 +0800

    Create privacy policy page

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -56,7 +56,8 @@ export async function updateSession(request: NextRequest) {
     !request.nextUrl.pathname.startsWith('/auth') &&
     !request.nextUrl.pathname.startsWith('/sign-up-success') &&
     !request.nextUrl.pathname.startsWith('/error') &&
-    !request.nextUrl.pathname.startsWith('/update-password')
+    !request.nextUrl.pathname.startsWith('/update-password') &&
+    !request.nextUrl.pathname.startsWith('/privacy-policy')
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
